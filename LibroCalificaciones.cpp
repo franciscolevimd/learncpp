7276c4866becd4e4383955d11c92879eaf2f70e6
Francisco Leví <franciscolevim@gmail.com>
2018-08-31 20:56:29 -0500

    [update] - Libro de calificaciones
    Se modifica la clase LibroCalificaciones para que su función miembro reciba un string C++.

--- LibroCalificaciones.cpp
+++ LibroCalificaciones.cpp
@@ -1,25 +1,46 @@
 // Define la clase LibroCalificaciones con una función miembro llamada mostrarMensaje;
+// La función miembro debe recibir un parámetro.
 // Crea un objeto LibroCalificaciones y llama a su función mostrarMensaje.
 #include <iostream>
 
 using std::cout; 
+using std::cin; 
 using std::endl;
+
+#include <string> // El programa usa una clase string estándar de C++
+using std::string;
+using std::getline;
+
 
 // Definción de la clase LibroCalificaciones
 class LibroCalificaciones
 {
 	public:
+		
 		// Función que muestra un mensaje de bienvenida para el usuario de LibroCalificaciones.
-		void mostrarMensaje()
+		void mostrarMensaje(string nombreCurso)
 		{
-			cout << "Bienvenido al Libro de calificaciones!" << endl;			
+			cout << "Bienvenido al Libro de calificaciones para \n" << nombreCurso << "!" << endl;
 		} // Fin de la función mostrarMensaje
+
 }; // Fin de la clase LibroCalificaciones
 
 // La función main empieza la ejecución del programa.
 int main()
 {
-	LibroCalificaciones miLibroCalificaciones; // Crea un objeto LibroCalificaciones.
-	miLibroCalificaciones.mostrarMensaje(); // Llama a la función mostrarMensaje del objeto.
+	
+	string nombreDelCurso; // Cadena de caracteres que almacena el nombre del curso.
+	LibroCalificaciones miLibroCalificaciones; // Crea un objeto LibroCalificaciones llamado miLibroCalificaciones
+
+	// Pide y recibe el nombre del curso como entrada.
+	cout << "Escriba el nombre del curso:" << endl;
+	getline(cin, nombreDelCurso); // Lee el nombre de un curso con espacios en blanco.
+	cout << endl; // Imprime un linea en blanco.
+
+	// Llama a la función mostrarMensaje de miLibroCalificaciones.
+	// y pasa nombreDelCurso como argumento.
+	miLibroCalificaciones.mostrarMensaje(nombreDelCurso);
+
 	return 0;
+
 } // Fin de main
